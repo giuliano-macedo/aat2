0748eac8b3fb683b6bb98bfd7e1b850d7179dd99
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-05-14 16:53:15 -0400

    trying

--- mochila.py
+++ mochila.py
@@ -1,5 +1,10 @@
-w=150
-items={(1,5),(2,4),(4,3),(14,6)}
-def mochila(n):
-	if n>150:return 0
-	return max()
+items={(1,5),(2,4),(4,3),(14,6)} #valor -> peso
+def mochila(w):
+	if w<=0:return -float("inf")
+	if w==4:return 2
+	a= max([valor+mochila(w-peso) for valor,peso in items])
+	return a
+if __name__=="__main__":
+	print(mochila(6)) #14
+	print(mochila(15)) #20
+	print(mochila(20)) #21
